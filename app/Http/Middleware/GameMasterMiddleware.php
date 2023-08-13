2554ae21de2e4d40d73cc508cbf5e91f0acd7172
tom <info@tommaessen.nl>
2023-08-13 21:47:07 +0200

    Bugfix

--- app/Http/Middleware/GameMasterMiddleware.php
+++ app/Http/Middleware/GameMasterMiddleware.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Player;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,6 +12,9 @@ class GameMasterMiddleware
     public function handle(Request $request, Closure $next): Response
     {
         if (session()->has('player') && session('player')->is_gamemaster) {
+            $player = Player::find(session('player')->id);
+            session(['player' => $player]);
+            
             return $next($request);
         }
 
--- app/Http/Middleware/LobbyMiddleware.php
+++ app/Http/Middleware/LobbyMiddleware.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Lobby;
+use App\Models\Player;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,6 +13,12 @@ class LobbyMiddleware
     public function handle(Request $request, Closure $next): Response
     {
         if (!session()->has('lobby') || !session()->has('player')) {
+            $lobby = Lobby::find(session('lobby')->id);
+            session(['lobby' => $lobby]);
+
+            $player = Player::find(session('player')->id);
+            session(['player' => $player]);
+
             return redirect()->route('index');
         }
 
